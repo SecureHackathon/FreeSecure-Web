afdb86715a7db01fa55d885f685edcb9a17cd682
Lebohang Danster <ldanster@gmail.com>
2014-06-28 14:01:29 +0200

    Removed fake camera

--- SecureWeb/SecureWeb/Controllers/CameraController.cs
+++ SecureWeb/SecureWeb/Controllers/CameraController.cs
@@ -12,27 +12,27 @@ namespace SecureWeb.Controllers
     {
         public CameraController()
             : base() {
-            Camera cam = new Camera {
-                Name = "Cam1",
-                Active = false,
-                DateCreated = DateTime.Now
-            };
+            //Camera cam = new Camera {
+            //    Name = "Cam1",
+            //    Active = false,
+            //    DateCreated = DateTime.Now
+            //};
 
-            Camera cam1 = new Camera {
-                Name = "Cam2",
-                Active = false,
-                DateCreated = DateTime.Now
-            };
+            //Camera cam1 = new Camera {
+            //    Name = "Cam2",
+            //    Active = false,
+            //    DateCreated = DateTime.Now
+            //};
 
-            Camera cam2 = new Camera {
-                Name = "Cam3",
-                Active = false,
-                DateCreated = DateTime.Now
-            };
+            //Camera cam2 = new Camera {
+            //    Name = "Cam3",
+            //    Active = false,
+            //    DateCreated = DateTime.Now
+            //};
 
-            _repository.Save<Camera>(cam);
-            _repository.Save<Camera>(cam1);
-            _repository.Save<Camera>(cam2);
+            //_repository.Save<Camera>(cam);
+            //_repository.Save<Camera>(cam1);
+            //_repository.Save<Camera>(cam2);
         }
         
         public IEnumerable<Camera> Get() {
