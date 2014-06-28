142d89080f9a9ffef0ca04d5ec0dd6e40757f264
Lebohang Danster <ldanster@gmail.com>
2014-06-28 16:18:03 +0200

    Adding fix for image post

--- SecureWeb/SecureWeb/Controllers/CameraController.cs
+++ SecureWeb/SecureWeb/Controllers/CameraController.cs
@@ -38,6 +38,10 @@ namespace SecureWeb.Controllers
         public IEnumerable<Camera> Get() {
             return _repository.GetAll<Camera>();
         }
+
+        public void Post() {
+
+        }
         
     }
 }
--- SecureWeb/SecureWeb/Controllers/ImageController.cs
+++ SecureWeb/SecureWeb/Controllers/ImageController.cs
@@ -16,49 +16,55 @@ using System.Web.Http;
 namespace SecureWeb.Controllers {
     public class ImageController :BaseApiController {
 
-        public void Post(string id) {
-            //var result = new HttpResponseMessage(HttpStatusCode.OK);
-            String fullPath="";
-            //if ( Request.Content.IsMimeMultipartContent() ) {
-            //    Request.Content.ReadAsMultipartAsync<MultipartMemoryStreamProvider>(new MultipartMemoryStreamProvider()).ContinueWith(( task ) => {
-            //        MultipartMemoryStreamProvider provider = task.Result;
-            //        foreach ( HttpContent content in provider.Contents ) {
-            //            Stream stream = content.ReadAsStreamAsync().Result;
-            //            Image image = Image.FromStream(stream);
-            //            var testName = content.Headers.ContentDisposition.Name;
-            //            String filePath = HostingEnvironment.MapPath("~/Images/");
-            //            String fileName = id + ".jpg";
-            //            fullPath = Path.Combine(filePath, fileName);
-            //            image.Save(fullPath);
-                        
-            //        }
-            //    });
+        public ImageController() {
+            ImageModel img = new ImageModel();
+            img.Url = "http://catthoughtalog.tumblr.com/image/17235419661";
+            ImageModel img2 = new ImageModel();
+            img2.Url = "http://catthoughtalog.tumblr.com/image/17235419661";
+            _repository.Save<ImageModel>(img);
+            _repository.Save<ImageModel>(img2);
+            _repository.Save<ImageModel>(img);
 
-                var result = new HttpResponseMessage(HttpStatusCode.OK);
-                if ( Request.Content.IsMimeMultipartContent() ) {
-                    StreamContent content = (StreamContent)Request.Content;
-                    Task<Stream> task = content.ReadAsStreamAsync();
-                    Stream readOnlyStream = task.Result;
-                    Byte[] buffer = new Byte[readOnlyStream.Length];
-                    readOnlyStream.Read(buffer, 0, buffer.Length);
-                    MemoryStream memoryStream = new MemoryStream(buffer);
-                    Image image = Image.FromStream(memoryStream);
-                    String path = HostingEnvironment.MapPath("~/Images/");
-                    String fileName = id + ".jpg";
-                    fullPath = Path.Combine(path, fileName);
-                    image.Save(fullPath);
-                    ImageModel img = new ImageModel();
-                    img.Url = fullPath;
-                    _repository.Save<ImageModel>(img);
-                } else {
-                    throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotAcceptable, "This request is not properly formatted"));
-                }
-                
-            }   
+        }
+
+        public async Task<HttpResponseMessage> Post() { // Check whether the POST operation is MultiPart? 
+            if (!Request.Content.IsMimeMultipartContent()) {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType); 
+            }
+            // Prepare CustomMultipartFormDataStreamProvider in which our multipart form 
+            // data will be loaded. 
+            string fileSaveLocation = HttpContext.Current.Server.MapPath("~/App_Data"); 
+            CustomMultipartFormDataStreamProvider provider = new CustomMultipartFormDataStreamProvider(fileSaveLocation); 
+            List<string> files = new List<string>(); 
+            try { 
+                // Read all contents of multipart message into CustomMultipartFormDataStreamProvider. 
+                await Request.Content.ReadAsMultipartAsync(provider); 
+                foreach (MultipartFileData file in provider.FileData) { 
+                    files.Add(Path.GetFileName(file.LocalFileName)); 
+                } 
+                // Send OK Response along with saved file names to the client. 
+                return Request.CreateResponse(HttpStatusCode.OK, files); 
+            } catch (System.Exception e) { 
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e); 
+            } 
+        }
 
         public IEnumerable<ImageModel> Get() {
-            return _repository.GetAll<ImageModel>().Where(o => o.Viewed == false);
+            var newImages = _repository.GetAll<ImageModel>().Where(o => o.Viewed == false);
+            var allImages = _repository.GetAll<ImageModel>();
+            foreach ( ImageModel img in allImages ) {
+                img.Viewed = true;
+                _repository.Update<ImageModel>(img);
+            }
+            return newImages;
         }
-       
+
+        public class CustomMultipartFormDataStreamProvider : MultipartFormDataStreamProvider { 
+            public CustomMultipartFormDataStreamProvider(string path) : base(path) { } 
+            
+            public override string GetLocalFileName(HttpContentHeaders headers) { 
+                return headers.ContentDisposition.FileName.Replace("\"", string.Empty); 
+            } 
+        }
     }
 }
